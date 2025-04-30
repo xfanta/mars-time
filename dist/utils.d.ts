3ee79e0752c0fb79fb0e0c083293883f0fa5b37a
Michal Fanta <michal.fanta@xfanta.cz>
2025-04-30 23:03:52 +0200

    feat: add getSolOfYear function for calculating Martian sol of the year

--- dist/utils.d.ts
+++ dist/utils.d.ts
@@ -6,3 +6,9 @@
  * @returns Angle in radians
  */
 export declare function deg2rad(deg: number): number;
+/**
+* Returns the sol of the year (day number in the current Martian year) from Mars Sol Date (MSD).
+* @param msd Mars Sol Date
+* @returns Sol of the year (1–669)
+*/
+export declare function getSolOfYear(msd: number): number;
